0070135db5a432c201991326e338783373d4e33c
xiaocheng <xiaoch2010@gmail.com>
2022-05-08 17:16:24 +0800

    add verify test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use cmmr::helper;
 use merkle_mountain_range::mmr;
 use std::env;
 use std::fmt;
 use serde::{Deserialize, Serialize};
+
+use cmmr::Merge;
+use merkle_mountain_range::mmr::merge;
+struct MergeHash;
+impl Merge for MergeHash {
+    type Item = [u8; 32];
+    fn merge(lhs: &Self::Item, rhs: &Self::Item) -> Self::Item {
+        merge(lhs, rhs)
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Peak {
@@ -11,8 +20,8 @@ pub struct Peak {
     position: String,
 }
 
-#[serde(rename_all = "camelCase")]
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct NodeEntities {
     pub node_entities: Vec<Peak>,
 }
@@ -75,6 +84,7 @@ impl fmt::Display for CheckPointInfo {
 
 #[tokio::main]
 async fn main() {
+    // ############## checkpoint #################
     let args: Vec<String> = env::args().collect();
     let url = args[1].clone();
     let block_num = args[2].parse::<u64>().unwrap();
@@ -99,31 +109,44 @@ async fn main() {
     }).collect();
 
     let mmr_root = mmr::bag_rhs_peaks(targetpeak_hashs).unwrap();
-    let checkPoint = CheckPointInfo {
+    let checkpoint = CheckPointInfo {
         block_number: block_num,
         position: target_pos,
         peaks: peaks.clone(),
         mmr_root: hex::encode(mmr_root),
     };
+    println!("{}", checkpoint);
+    // ############## checkpoint end #################
 
-    println!("{}", checkPoint);
-
-    // cal proof
+    // ############## generate mmr proof #################
+    // 1. cal proof positions
     let check_pos = mmr::leaf_index_to_pos(block_num - 1);
-    let mmr_size = helper::leaf_index_to_mmr_size(block_num);
+    let mmr_size = mmr::leaf_index_to_pos(block_num);
     let (merkle_proof_pos, peaks_pos, peak_pos) = mmr::gen_proof_positions(check_pos, mmr_size);
+
+    // 2. query hash by positions from thegraph
     let proof_peaks = query_positions(&url, peaks_pos).await;
     let merkle_proof_nodes = query_positions(&url, merkle_proof_pos).await;
+    let mut merkle_proof: Vec<[u8; 32]> = merkle_proof_nodes.iter().map(|x| { x.1 }).collect();
 
-    let merkle_proof = merkle_proof_nodes.iter().map(|x| {
-        x.1
-    }).collect();
+    // we need reverse here because the response from thegraph is sorted by id
+    merkle_proof.reverse();
 
-    // 3. gen proof
+    // 3. gen proof by hashes
     let mmr_proof = mmr::gen_proof(merkle_proof, proof_peaks, peak_pos);
     println!("mmr proof");
-    for (idx, v) in mmr_proof.iter().enumerate() {
+    for (_, v) in mmr_proof.iter().enumerate() {
         println!("{:?}", hex::encode(v));
     }
+    // ############## generate mmr proof end #################
+
+    // ############## verify mmr proof #################
+    // verify proof
+    let proof = cmmr::MerkleProof::<[u8;32], MergeHash>::new(mmr_size, mmr_proof);
+    let checked_position = query_positions(&url, vec![check_pos]).await;
+    let checked = checked_position[0].1;
+    // this should return true
+    println!("{:?}", proof.verify(mmr_root, vec![(check_pos, checked)]).unwrap());
+    // ############## verify mmr proof end #################
 }
 
